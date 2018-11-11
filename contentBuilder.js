c1d461c2eec449f807aa1d1f1d5834b05574b307
Frank-Dippnall <44258571+Frank-Dippnall@users.noreply.github.com>
2018-11-11 09:58:37 +0000

    Update contentBuilder.js

--- contentBuilder.js
+++ contentBuilder.js
@@ -1,18 +1,3 @@
-function drawVideo(vid){
-    
-}
-function addVideos(){
-    $.get("clips.xml", function(xml){
-        var xmlDoc = $.parseXML(xml);
-        $xml = $(xmlDoc);
-        $video = $xml.find("show");
-        $video.each(function(){
-            $("#vids").append("<p>"+"<h3>"+$(this).find("title").text()+"</h3>"+"</p>");
-        });
-        
-    });
-    
-}
 
 $(document).ready(function(){
     $.get("navigation.html", function(data){
@@ -25,6 +10,12 @@ $(document).ready(function(){
             });
         });
     });
-    addVideos();
-    
+    $.get("clips.xml", function(xml){
+        var xmlDoc = $.parseXML(xml);
+        $xml = $(xmlDoc);
+        $video = $xml.find("show");
+        $video.each(function(){
+            $("#vids").append("<p>"+"<h3>"+$(this).find("title").text()+"</h3>"+"</p>");
+        });
+    });
 });
