17a54416256d6123b8e7567dd5d7190b1e12bc17
Frank-Dippnall <44258571+Frank-Dippnall@users.noreply.github.com>
2018-11-11 02:07:11 +0000

    load test

--- contentBuilder.js
+++ contentBuilder.js
@@ -3,7 +3,5 @@ $(document).ready(function(){
     $.get("navigation.html", function(data){
         $(this).append(data);
     });
-    $.get("footer.html", function(data){
-        $(this).append(data);  
-    });
+    $("body").load("footer.html");
 });
