89845007b9797af46f1ea271ca62f84a7e9f4936
Frank-Dippnall <44258571+Frank-Dippnall@users.noreply.github.com>
2018-11-11 02:41:26 +0000

    update 42

--- contentBuilder.js
+++ contentBuilder.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function(){
     $.get("navigation.html", function(data){
-        $("body").append(data);
+        $("body").prepend(data);
         $.get("footer.html", function(data2){
             $("body").append(data2);  
         });
