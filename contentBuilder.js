b499e39229ed020237c39c6750ce9cc664078ca7
Frank-Dippnall <44258571+Frank-Dippnall@users.noreply.github.com>
2018-11-11 02:21:31 +0000

    added br

--- contentBuilder.js
+++ contentBuilder.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     $.get("navigation.html", function(data){
         $("body").append(data);
     });
+    $("body").append("<br><br>");
     $.get("footer.html", function(data){
         $("body").append(data);  
     });
