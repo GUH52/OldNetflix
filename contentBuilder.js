c2af820f5af6ad26c17aa01eb89dcebd78bb85ef
Frank-Dippnall <44258571+Frank-Dippnall@users.noreply.github.com>
2018-11-11 09:35:07 +0000

    Update contentBuilder.js

--- contentBuilder.js
+++ contentBuilder.js
@@ -1,9 +1,25 @@
+function drawVideo(vid){
+    $("#vids").append(vid);
+}
+function addVideos(){
+    $.get("clips.xml", function(xml){
+        var xmlDoc = $.parseXML(xml);
+        $xml = $(xmlDoc);
+        $video = $xml.find("show");
+        drawVideo($video);
+    });
+    
+}
 
 $(document).ready(function(){
     $.get("navigation.html", function(data){
         $("body").prepend(data);
+        $("body").append("<section id=\"vids\"></section>");
         $.get("footer.html", function(data2){
-            $("body").append(data2);  
+            $("body").append(data2);
+            $.get("ads.html", function(data3){
+                $("body").append(data3);
+            });
         });
     });
     
